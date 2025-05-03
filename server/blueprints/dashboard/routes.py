da1fb8348e6f92f38accf010cce222254f0e4cde
XiaoqinFAN <24055569@student.uwa.edu.au>
2025-05-03 21:24:26 +0800

    fix merge undetected conflict

--- server/blueprints/dashboard/routes.py
+++ server/blueprints/dashboard/routes.py
@@ -5,11 +5,12 @@ from server.blueprints.dashboard.logic import fetch_weather_forecast
 dashboard_bp = Blueprint("dashboard", __name__, template_folder="templates")
 
 
-<<<<<<< HEAD
+# <<<<<<< HEAD
+
 @dashboard_bp.route("/dashboard", methods=["GET", "POST"])
-=======
-@dashboard_bp.route("/", methods=["GET"])
->>>>>>> 78f5ef41f81d91b12f819b002c260a5e5ce20d2b
+# =======
+# @dashboard_bp.route("/", methods=["GET"])
+# >>>>>>> 78f5ef41f81d91b12f819b002c260a5e5ce20d2b
 @login_required
 def index():
     weather_forecast = fetch_weather_forecast("Perth", days=5)
