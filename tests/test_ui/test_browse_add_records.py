dee6338f932a97db12ce7c62fd84a9cff414041d
Samuel <zimingw.ol@outlook.com>
2025-05-16 17:47:56 +0800

    feat: remove unused pytz import from browse add records tests

--- tests/test_ui/test_browse_add_records.py
+++ tests/test_ui/test_browse_add_records.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import pytz
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
