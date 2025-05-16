14311c7c15a9624ac49703fb0d69de8a949e8e88
Samuel <zimingw.ol@outlook.com>
2025-05-16 16:44:36 +0800

    feat: add timezone option for Chrome WebDriver in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -79,6 +79,7 @@ def chrome_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--timezone=Australia/Perth")
 
     driver = webdriver.Chrome(options=options)
     yield driver
